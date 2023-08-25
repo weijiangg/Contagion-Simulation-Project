2a7e65b8073c075cc1a0ff23d3f85de44e967da0
Wei Jiang <111544350+weijiangg@users.noreply.github.com>
2023-08-25 15:40:43 -0400

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,22 @@
+"""Constants used through the simulation."""
+
+BOUNDS_WIDTH: int = 800
+MAX_X: float = BOUNDS_WIDTH / 2
+MIN_X: float = -MAX_X
+VIEW_WIDTH: int = BOUNDS_WIDTH + 20
+
+BOUNDS_HEIGHT: int = 800
+MAX_Y: float = BOUNDS_HEIGHT / 2
+MIN_Y: float = -MAX_Y
+VIEW_HEIGHT: int = BOUNDS_HEIGHT + 20
+
+CELL_RADIUS: int = 15
+CELL_COUNT: int = 50
+CELL_SPEED: float = 5.0
+
+VULNERABLE: int = 0 
+INFECTED: int = 1
+BEGIN_INFECTED: int = 5
+IMMUNE: int = -1
+RECOVERY_PERIOD: int = 90
+BEGIN_IMMUNE: int = 3 
